aebca60e4045ddcda5201a88bb3040f92a60ea22
Spelchure <alperenbayramoglu2@gmail.com>
2022-01-15 16:10:52 +0300

    Initial commit

---
+++ src/asc_main.cc
@@ -0,0 +1,15 @@
+/*
+ * @file asc_main.cc
+ * @author Alperen BAYRAMOĞLU
+ * @date 2022
+ *
+ * @brief Application entry for arduino serial communication
+ *
+ */
+#include <iostream>
+
+int
+main(int argc, char **argv)
+{
+	std::cout << "Arduino Serial Communication.\n";
+}
